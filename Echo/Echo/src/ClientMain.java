b07f4b15cd3b8dfe87b908303e6abb739dc151be
ChembsTacoma <chembiang3@gmail.com>
2024-02-22 13:58:28 -0600

    Update ClientMain.java

--- Echo/Echo/src/ClientMain.java
+++ Echo/Echo/src/ClientMain.java
@@ -1,2 +1,33 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientMain {
+    public static void main(String[] args) {
+        try{
+            //connects to the server on the given IPAddress and port
+            Socket socket=new Socket("127.0.0.1",8000);
+
+            //creates a stream for writing objects to the server
+            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+            //creates a stream for reading objects from the server
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            String text="";
+            Scanner keyboard=new Scanner(System.in);
+            do{
+                System.out.print("Enter text to send to the server (\"exit\" to quit): ");
+                text=keyboard.next();
+                os.writeObject(text);
+                os.reset();
+                if (!text.equals("exit")) {
+                    String echo = (String) is.readObject();
+                    System.out.println("\t echo: " + echo);
+                }
+            }while(!text.equals("exit"));
+        }
+        catch (Exception error){
+            error.printStackTrace();
+        }
+    }
 }
