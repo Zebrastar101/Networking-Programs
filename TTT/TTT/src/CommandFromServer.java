ca2d889573d8fc31bcb14c0e78a773df1e40b2bc
Zahraa <zahraatfatehi@gmail.com>
2024-02-22 17:12:27 -0600

    Update ComandFromServer video

--- TTT/TTT/src/CommandFromServer.java
+++ TTT/TTT/src/CommandFromServer.java
@@ -9,5 +9,27 @@ public class CommandFromServer implements Serializable{
     //Command list
     public static final int CONNECT_AS_X = 0;
     public static final int CONNECT_AS_O = 1;
+    public static final int X_TURN = 2;
+    public static final int O_TURN = 3;
+    public static final int MOVE = 4;
+    public static final int X_WIN = 5;
+    public static final int O_WIN = 6;
+    public static final int TIE = 7;
+    public static final int RESTARt = 7;
+
+    public CommandFromServer(int command, String data){
+        this.command = command;
+        this.data = data;
+    }
+
+    public int getCommand(){
+        return command;
+    }
+
+    public String getData(){
+        return data;
+    }
+    
+
 
 }
