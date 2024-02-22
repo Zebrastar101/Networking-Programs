b0903098502e585e000f913ff3233b15f4669884
Zahraa <zahraatfatehi@gmail.com>
2024-02-22 16:54:54 -0600

    CommandFromClient code from video

--- TTT/TTT/src/CommandFromClient.java
+++ TTT/TTT/src/CommandFromClient.java
@@ -1,2 +1,25 @@
-public class CommandFromClient {
+
+import java.io.Serializable;
+public class CommandFromClient implements Serializable{
+    //The comand being sent to the server
+    private int command;
+    //Text data for the command
+    private String data ="";
+    //Command List
+    public static final int MOVE = 0;
+    public static final int RESTART = 1;
+
+    public CommandFromClient(int command, String data){
+        this.command = command;
+        this.data = data;
+    }
+
+    public int getCommand(){
+        return command;
+    }
+    public String getData(){
+        return data;
+    }
+
+
 }
