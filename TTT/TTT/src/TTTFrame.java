5a9166b8076027ded090f7325e7b256038c222e1
Zahraa <zahraatfatehi@gmail.com>
2024-02-23 09:54:23 -0600

    Update TTTFrame.java

--- TTT/TTT/src/TTTFrame.java
+++ TTT/TTT/src/TTTFrame.java
@@ -1,65 +1,76 @@
-
-import java.awt.*;
-import java.awt.event.KeyListener;
 import javax.swing.*;
-public class TTTFrame extends JFrame implements KeyListener{
-    //Display message
-    private String text="";
-    //the letter you are playing
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Collections;
+
+public class TTTFrame extends JFrame implements KeyListener {
+    // Display message
+    private String text = "";
+    // the letter you are playing as
     private char player;
-    //store the letter of the active player
-    private char turn='X';
-    //store all game data
-    private GameData gameData=null;
-    //output stream to the server
-    public TTTFrame(GameData gameData, char player) {
+    // stores all the game data
+    private GameData gameData = null;
+    // output stream to the server
+    ObjectOutputStream os;
+
+    public TTTFrame(GameData gameData, ObjectOutputStream os, char player)
+    {
         super("TTT Game");
-        //set attributes
+        // sets the attributes
         this.gameData = gameData;
+        this.os = os;
         this.player = player;
-        //add a key listener
+
+        // adds a KeyListener to the Frame
         addKeyListener(this);
-        //make closing the fram close the program
+
+        // makes closing the frame close the program
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        setSize(400, 460);
+        // Set initial frame message
+        if(player == 'X')
+            text = "Waiting for O to Connect";
+
+        setSize(400,460);
+        setResizable(false);
         setAlwaysOnTop(true);
         setVisible(true);
-
     }
-    public void paint(Graphics g) {
-        //draws background
+
+    public void paint(Graphics g)
+    {
+        // draws the background
         g.setColor(Color.BLACK);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        g.fillRect(0,0,getWidth(),getHeight());
 
-        //draws the display text to the screen
+        // draws the display text to the screen
         g.setColor(Color.RED);
-        g.setFont(new Font("Times New Roman", Font.BOLD, 30));
-        g.drawString(text+"dfasdf asdfas", 20, 55);
+        g.setFont(new Font("Times New Roman",Font.BOLD,30));
+        g.drawString(text,20,55);
 
-        //draw the tic-tac-toe grid lines to the screen
+        // draws the tic-tac-toe grid lines to the screen
         g.setColor(Color.RED);
-        for(int y=0; y<=1; y++) {
-            g.drawLine(0, (y+1)*133+60, getWidth(), (y+1)*133+60);
-        }
-        for(int x=0; x<=1; x++) {
-            g.drawLine((x+1)*133, 60, (x+1)*133, getHeight());
-        }
-        g.setFont(new Font("Times New Roman", Font.BOLD,70));
-        for(int r=0; r<gameData.getGrid().length; r++) {
-            for(int c=0; c<gameData.getGrid().length; c++) {
-                if(gameData.getGrid()[r][c] != ' ') {
-                    g.setColor(Color.RED);
-                    g.drawString(""+gameData.getGrid()[r][c], c*133+42, r*133+150);
-                }
-            }
-        }
+        for(int y =0;y<=1; y++)
+            g.drawLine(0,(y+1)*133+60,getWidth(),(y+1)*133+60);
+        for(int x =0;x<=1; x++)
+            g.drawLine((x+1)*133,60,(x+1)*133,getHeight());
 
+        // draws the player moves to the screen
+        g.setFont(new Font("Times New Roman",Font.BOLD,70));
+        for(int r=0; r<gameData.getGrid().length; r++)
+            for(int c=0; c<gameData.getGrid().length; c++)
+                g.drawString(""+gameData.getGrid()[r][c],c*133+42,r*133+150);
     }
+
     public void setText(String text) {
         this.text = text;
         repaint();
     }
+
+
     public void setTurn(char turn) {
         if(turn==player)
             text = "Your turn";
@@ -144,7 +155,4 @@ public class TTTFrame extends JFrame implements KeyListener{
     public void keyReleased(KeyEvent e) {
 
     }
-}
-
-
 }
