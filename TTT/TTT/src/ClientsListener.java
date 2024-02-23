1bda4f5d2a47d03dc58ad20c323fc0b5728698a1
ChembsTacoma <chembiang3@gmail.com>
2024-02-23 09:53:12 -0600

    Update ClientsListener.java

--- TTT/TTT/src/ClientsListener.java
+++ TTT/TTT/src/ClientsListener.java
@@ -14,12 +14,40 @@ public class ClientsListener implements Runnable{
 
     @Override
     public void run() {
-        try{
-            while(true){
-                //reads commands from the server
-                CommandFromServer cfs= (CommandFromServer)is.readObject();
-                //processes the received command
+        try
+        {
+            while(true)
+            {
+                CommandFromServer cfs = (CommandFromServer)is.readObject();
 
+                // processes the received command
+                if(cfs.getCommand() == CommandFromServer.X_TURN)
+                    frame.setTurn('X');
+                else if(cfs.getCommand() == CommandFromServer.O_TURN)
+                    frame.setTurn('O');
+                else if(cfs.getCommand() == cfs.MOVE)
+                {
+                    String data = cfs.getData();
+                    // pulls data for the move from the data field
+                    int c = data.charAt(0) - '0';
+                    int r = data.charAt(1) - '0';
+
+                    // changes the board and redraw the screen
+                    frame.makeMove(c,r,data.charAt(2));
+                }
+                // handles the various end game states
+                else if(cfs.getCommand() == CommandFromServer.TIE)
+                {
+                    frame.setText("Tie game.");
+                }
+                else if(cfs.getCommand() == CommandFromServer.X_WINS)
+                {
+                    frame.setText("X wins!");
+                }
+                else if(cfs.getCommand() == CommandFromServer.O_WINS)
+                {
+                    frame.setText("O wins!");
+                }
             }
         }
         catch(Exception error){
