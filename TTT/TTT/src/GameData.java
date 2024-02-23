840ddf644b46582ccbcd427197c26f49125fb953
Zahraa <zahraatfatehi@gmail.com>
2024-02-23 09:53:14 -0600

    Update GameData.java

--- TTT/TTT/src/GameData.java
+++ TTT/TTT/src/GameData.java
@@ -1,38 +1,48 @@
-public class GameData {
-    private char[][] grid={{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '} };
-    public char[][] getGrid() {
+public class GameData
+{
+    private char[][] grid = {{' ',' ', ' '},{' ',' ', ' '},{' ',' ', ' '}};
+
+    public char[][] getGrid()
+    {
         return grid;
     }
-    public void reset() {
-        for (int r=0; r<grid.length; r++) {
-            for (int c=0; c<grid.length; c++) {
-                grid[r][c] = ' ';
-            }
-        }
+
+    public void reset()
+    {
+
+        grid = new char[3][3];
+        for(int r=0;r<grid.length; r++)
+            for(int c=0; c<grid[0].length; c++)
+                grid[r][c]=' ';
     }
-    public boolean isCat(){
-        if(grid[0][0] != ' ' && grid[0][1] != ' ' && grid[0][2] != ' ' &&
-                grid[1][0] != ' ' && grid[1][1] != ' ' && grid[1][2] != ' '
-                && grid[2][0] != ' ' && grid[2][1] != ' ' && grid[2][2] != ' '){
+
+
+    public boolean isCat()
+    {
+        if(grid[0][0] !=' ' && grid[0][1] !=' ' && grid[0][2] !=' '
+                && grid[1][0] !=' ' && grid[1][1] !=' ' && grid[1][2] !=' '
+                && grid[2][0] !=' ' && grid[2][1] !=' ' && grid[2][2] !=' '
+                && !isWinner('X') && !isWinner('O'))
             return true;
-        }
-        else {
+        else
             return false;
-        }
     }
-    public boolean isWinner(char letter){
-        if((grid[0][0]==letter&&grid[0][1]==letter&&grid[0][2]==letter)||
-                (grid[1][0]==letter&&grid[1][1]==letter&&grid[1][2]==letter)||
-                (grid[2][0]==letter&&grid[2][1]==letter&&grid[2][2]==letter)||
-                (grid[0][0]==letter&&grid[1][0]==letter&&grid[2][0]==letter)||
-                (grid[0][1]==letter&&grid[1][1]==letter&&grid[2][1]==letter)||
-                (grid[0][2]==letter&&grid[1][2]==letter&&grid[2][2]==letter)||
-                (grid[0][0]==letter&&grid[1][1]==letter&&grid[2][2]==letter)||
-                (grid[0][2]==letter&&grid[1][1]==letter&&grid[2][0]==letter)){
+
+    public boolean isWinner(char letter)
+    {
+        if((grid[0][0] ==letter && grid[0][1] ==letter && grid[0][2] ==letter)
+                || (grid[1][0] ==letter && grid[1][1] ==letter && grid[1][2] ==letter)
+                || (grid[2][0] ==letter && grid[2][1] ==letter && grid[2][2] ==letter)
+
+                || (grid[0][0] ==letter && grid[1][0] ==letter && grid[2][0] ==letter)
+                || (grid[0][1] ==letter && grid[1][1] ==letter && grid[2][1] ==letter)
+                || (grid[0][2] ==letter && grid[1][2] ==letter && grid[2][2] ==letter)
+
+                || (grid[0][0] ==letter && grid[1][1] ==letter && grid[2][2] ==letter)
+                || (grid[0][2] ==letter && grid[1][1] ==letter && grid[2][0] ==letter))
             return true;
-        }
-        else {
+        else
             return false;
-        }
     }
+
 }
