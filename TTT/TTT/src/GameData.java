19c72d0af672c0dad0b4e7890398bef5cdd4bff3
Zahraa <zahraatfatehi@gmail.com>
2024-02-23 07:54:02 -0600

    Update Game data from video

--- TTT/TTT/src/GameData.java
+++ TTT/TTT/src/GameData.java
@@ -20,5 +20,19 @@ public class GameData {
             return false;
         }
     }
-    
+    public boolean isWinner(char letter){
+        if((grid[0][0]==letter&&grid[0][1]==letter&&grid[0][2]==letter)||
+                (grid[1][0]==letter&&grid[1][1]==letter&&grid[1][2]==letter)||
+                (grid[2][0]==letter&&grid[2][1]==letter&&grid[2][2]==letter)||
+                (grid[0][0]==letter&&grid[1][0]==letter&&grid[2][0]==letter)||
+                (grid[0][1]==letter&&grid[1][1]==letter&&grid[2][1]==letter)||
+                (grid[0][2]==letter&&grid[1][2]==letter&&grid[2][2]==letter)||
+                (grid[0][0]==letter&&grid[1][1]==letter&&grid[2][2]==letter)||
+                (grid[0][2]==letter&&grid[1][1]==letter&&grid[2][0]==letter)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
