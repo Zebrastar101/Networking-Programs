7bea2a837aaf2f8b6bf57e8d71d24afada909f08
ChembsTacoma <chembiang3@gmail.com>
2024-02-23 09:54:59 -0600

    Update ServerMain.java

--- TTT/TTT/src/ServerMain.java
+++ TTT/TTT/src/ServerMain.java
@@ -6,27 +6,40 @@ import java.net.Socket;
 public class ServerMain {
     public static void main(String[] args) {
         try{
-            //creates socket that allows connection on port 8001
-            ServerSocket serverSocket=new ServerSocket(8001);
-            //allow X to connect and build streams to&from X
-            Socket xConnection = serverSocket.accept();
-            ObjectOutputStream xos = new ObjectOutputStream(xConnection.getOutputStream());
-            ObjectInputStream xis = new ObjectInputStream(xConnection.getInputStream());
-            //CommandFromServer will be an error until me and zara merge
-            //send a command
+            // creates a socket that allows connections on port 8001
+            ServerSocket serverSocket = new ServerSocket(8001);
+
+            // allow X to connect and build streams to / from X
+            Socket xCon = serverSocket.accept();
+            ObjectOutputStream xos = new ObjectOutputStream(xCon.getOutputStream());
+            ObjectInputStream xis = new ObjectInputStream(xCon.getInputStream());
+
+            // Lets the client know they are the X player
             xos.writeObject(new CommandFromServer(CommandFromServer.CONNECTED_AS_X,null));
-            System.out.println("X has connected");
+            System.out.println("X has Connected.");
 
-            ServersListener sl = new ServersListener();
+            // Creates a Thread to listen to the X client
+            ServersListener sl = new ServersListener(xis,xos,'X');
+            Thread t = new Thread(sl);
+            t.start();
 
-            //allow O to connect and build streams to&from O
-            Socket oConnection = serverSocket.accept();
-            ObjectOutputStream oos = new ObjectOutputStream(oConnection.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(oConnection.getInputStream());
-            //CommandFromServer will be an error until me and zara merge
-            //send a command
+            // allow O to connect and build streams to / from O
+            Socket oCon = serverSocket.accept();
+            ObjectOutputStream oos = new ObjectOutputStream(oCon.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(oCon.getInputStream());
+
+            // Lets the client know they are the X player
             oos.writeObject(new CommandFromServer(CommandFromServer.CONNECTED_AS_O,null));
-            System.out.println("O has connected");
+            System.out.println("O has Connected.");
+
+            // Creates a Thread to listen to the X client
+            sl = new ServersListener(ois,oos,'O');
+            t = new Thread(sl);
+            t.start();
+
+
+            xos.writeObject(new CommandFromServer(CommandFromServer.X_TURN,null));
+            oos.writeObject(new CommandFromServer(CommandFromServer.X_TURN,null));
         }
         catch (Exception error){
             error.printStackTrace();
