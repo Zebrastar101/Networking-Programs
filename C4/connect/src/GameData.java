316a56b8df495fb3d3951a9e9598d4cec7760626
Zahraa <zahraatfatehi@gmail.com>
2024-02-24 14:37:19 -0600

    Update GameData.java

--- C4/connect/src/GameData.java
+++ C4/connect/src/GameData.java
@@ -1,6 +1,8 @@
 public class GameData
 {
-    private char[][] grid = 
+    private char[][] grid ={{' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' '}
+            ,{' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' '}
+            };
 
     public char[][] getGrid()
     {
@@ -10,7 +12,7 @@ public class GameData
     public void reset()
     {
 
-        grid = new char[3][3];
+        grid = new char[6][7];
         for(int r=0;r<grid.length; r++)
             for(int c=0; c<grid[0].length; c++)
                 grid[r][c]=' ';
@@ -19,10 +21,18 @@ public class GameData
 
     public boolean isCat()
     {
-        if(grid[0][0] !=' ' && grid[0][1] !=' ' && grid[0][2] !=' '
-                && grid[1][0] !=' ' && grid[1][1] !=' ' && grid[1][2] !=' '
-                && grid[2][0] !=' ' && grid[2][1] !=' ' && grid[2][2] !=' '
-                && !isWinner('X') && !isWinner('O'))
+        if(grid[0][0] !=' ' && grid[0][1] !=' ' && grid[0][2] !=' ' &&
+                grid[0][3] !=' ' && grid[0][4] !=' ' && grid[0][5] !=' ' && grid[0][6] !=' '&&
+                grid[1][0] !=' ' && grid[1][1] !=' ' && grid[1][2] !=' '&& grid[1][3] !=' '&&
+                grid[1][4] !=' '&& grid[1][5] !=' '&& grid[1][6] !=' '&&
+                grid[2][0] !=' '&&grid[2][1] !=' '&& grid[2][3] !=' '&& grid[2][2] !=' '&&
+                grid[2][4] !=' '&& grid[2][5] !=' '&& grid[2][6] !=' '&&
+                grid[3][0] !=' '&& grid[3][1] !=' '&& grid[3][2] !=' '&&
+                grid[3][3] !=' '&& grid[3][4] !=' '&& grid[3][5] !=' '&& grid[3][6] !=' ' &&
+                grid[4][0] !=' '&& grid[4][1] !=' '&& grid[4][2] !=' '&& grid[4][3] !=' '&&
+                grid[4][4] !=' '&& grid[4][5] !=' '&& grid[4][6] !=' '&&
+                grid[5][0] !=' '&& grid[5][1] !=' '&& grid[5][2] !=' '&& grid[5][3] !=' '&&
+                grid[5][4] !=' '&& grid[5][5] !=' '&& grid[5][6] !=' '&&isWinner('X')==false&&isWinner('O')==false)
             return true;
         else
             return false;
@@ -30,19 +40,45 @@ public class GameData
 
     public boolean isWinner(char letter)
     {
-        if((grid[0][0] ==letter && grid[0][1] ==letter && grid[0][2] ==letter)
-                || (grid[1][0] ==letter && grid[1][1] ==letter && grid[1][2] ==letter)
-                || (grid[2][0] ==letter && grid[2][1] ==letter && grid[2][2] ==letter)
-
-                || (grid[0][0] ==letter && grid[1][0] ==letter && grid[2][0] ==letter)
-                || (grid[0][1] ==letter && grid[1][1] ==letter && grid[2][1] ==letter)
-                || (grid[0][2] ==letter && grid[1][2] ==letter && grid[2][2] ==letter)
-
-                || (grid[0][0] ==letter && grid[1][1] ==letter && grid[2][2] ==letter)
-                || (grid[0][2] ==letter && grid[1][1] ==letter && grid[2][0] ==letter))
-            return true;
-        else
-            return false;
+        //Check horizontal win row one
+        // Check horizontal win
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 4; c++) {
+                if (grid[r][c] == letter &&
+                        grid[r][c + 1] == letter &&
+                        grid[r][c + 2] == letter &&
+                        grid[r][c + 3] == letter) {
+                    return true;
+                }
+            }
+        }
+        //Check vertical win
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 7; j++)
+            {
+                if(grid[i][j] == letter && grid[i+1][j] == letter && grid[i+2][j] == letter && grid[i+3][j] == letter)
+                    return true;
+            }
+        }
+        //Check diagonal win
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                if(grid[i][j] == letter && grid[i+1][j+1] == letter && grid[i+2][j+2] == letter && grid[i+3][j+3] == letter)
+                    return true;
+            }
+        }
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 3; j < 7; j++)
+            {
+                if(grid[i][j] == letter && grid[i+1][j-1] == letter && grid[i+2][j-2] == letter && grid[i+3][j-3] == letter)
+                    return true;
+            }
+        }
+        return false;
     }
 
 }
