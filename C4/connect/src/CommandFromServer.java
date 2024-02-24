1330f44c57d9c961948e282ee1ef3c1f373dbfe7
Zahraa <zahraatfatehi@gmail.com>
2024-02-24 14:51:49 -0600

    Create CommandFromServer.java

---
+++ C4/connect/src/CommandFromServer.java
@@ -0,0 +1,35 @@
+
+import java.io.Serializable;
+public class CommandFromServer implements Serializable{
+    //The command being sent to the client
+    private int command;
+    //Text data for command
+    private String data="";
+
+    //Command list
+    public static final int CONNECT_AS_Y = 0;
+    public static final int CONNECT_AS_R = 1;
+    public static final int Y_TURN = 2;
+    public static final int R_TURN = 3;
+    public static final int MOVE = 4;
+    public static final int Y_WIN = 5;
+    public static final int R_WIN = 6;
+    public static final int TIE = 7;
+
+
+    public CommandFromServer(int command, String data){
+        this.command = command;
+        this.data = data;
+    }
+
+    public int getCommand(){
+        return command;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+
+
+}
