326669e1ae66fcfe0a795300678d670974f3fcc2
ChembsTacoma <chembiang3@gmail.com>
2024-02-24 18:14:28 -0600

    Update C4Frame.java

--- C4/connect/src/C4Frame.java
+++ C4/connect/src/C4Frame.java
@@ -1,2 +1,20 @@
-public class C4Frame {
+import javax.swing.*;
+import java.awt.*;
+
+public class C4Frame extends JFrame {
+    JButton button00 = new JButton();
+    public C4Frame(){
+        super("C4");
+        setSize(400,460);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        getContentPane().setBackground(Color.BLUE);
+
+
+        setVisible(true);
+    }
+
+
+
 }
