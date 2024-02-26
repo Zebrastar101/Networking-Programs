3a9c8a13782c68806d3aa76013b5725b67f2a2c0
Zahraa <zahraatfatehi@gmail.com>
2024-02-26 09:20:50 -0600

    Update C4Frame.java

--- C4/connect/src/C4Frame.java
+++ C4/connect/src/C4Frame.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class C4Frame extends JFrame implements WindowListener, MouseListener{
-    
+public class C4Frame extends JFrame implements WindowListener, MouseListener {
+
     public C4Frame(){
         super("C4");
         setSize(600,660);
@@ -16,5 +20,63 @@ public class C4Frame extends JFrame implements WindowListener, MouseListener{
     }
 
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
 
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
 }
