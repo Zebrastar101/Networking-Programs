862ceeae8667c56464d43f1c91f50b334e701658
Zahraa <zahraatfatehi@gmail.com>
2024-02-26 09:14:05 -0600

    Update C4Frame.java

--- C4/connect/src/C4Frame.java
+++ C4/connect/src/C4Frame.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class C4Frame extends JFrame implements WindowListener, MouseListener{
-    JButton button00 = new JButton();
+    
     public C4Frame(){
         super("C4");
         setSize(600,660);
