e54ea113f9884df2cafa86f58a899748b1fcf4c1
ChembsTacoma <chembiang3@gmail.com>
2024-02-28 09:32:39 -0600

    Update C4Frame.java

--- C4/connect/src/C4Frame.java
+++ C4/connect/src/C4Frame.java
@@ -160,14 +160,7 @@ public class C4Frame extends JFrame implements WindowListener, MouseListener {
         //default
         g.setColor(Color.PINK);
         g.fillRect(0,0,getWidth(),getHeight());
-        for(int c=0; c<6; c++){
-            int y=40+(c*80);
-            for(int i=0; i<7; i++){
-                g.setColor(Color.WHITE);
-                g.drawOval(20 + (i*80),y, 60, 60);
-                g.fillOval(20 + (i*80),y,60,60);
-            }
-        }
+
         //text
         g.setColor(Color.RED);
         g.setFont(new Font("Times New Roman",Font.BOLD,30));
@@ -178,6 +171,11 @@ public class C4Frame extends JFrame implements WindowListener, MouseListener {
         for(int c=0; c<6; c++){
             int y=40+(c*80);
             for(int i=0; i<7; i++){
+                if(gameData.getGrid()[c][i] == ' '){
+                    g.setColor(Color.WHITE);
+                    g.drawOval(20 + (i*80),y, 60, 60);
+                    g.fillOval(20 + (i*80),y,60,60);
+                }
                 if(gameData.getGrid()[c][i] == 'R'){
                     g.setColor(Color.RED);
                     g.drawOval(20 + (i*80),y, 60, 60);
