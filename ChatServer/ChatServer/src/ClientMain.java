13eb9009a7f4b3d3dea98756ffe29dff3a1d410a
Zahraa <zahraatfatehi@gmail.com>
2024-03-04 09:33:45 -0600

    Update ClientMain.java

--- ChatServer/ChatServer/src/ClientMain.java
+++ ChatServer/ChatServer/src/ClientMain.java
@@ -1,2 +1,32 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class ClientMain {
+    public static void main(String[] args) {
+        try{
+
+            //create a connection to server
+            Socket socket = new Socket("127.0.0.1", 8001);
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+
+            //determine if playing as X or O
+            CommandFromServer cfs = (CommandFromServer)is.readObject();
+            ChatServer frame;
+
+            if (cfs.getCommand() == CommandFromServer.CONNECT_AS_Y){
+                frame = new C4Frame(gameData, os,'Y');
+            }
+            else{
+                frame = new C4Frame(gameData,os, 'R');
+            }
+            ClientsListener cl = new ClientsListener(is, os, frame);
+            Thread t = new Thread(cl);
+            t.start();
+        }
+        catch(Exception error){
+            error.printStackTrace();
+        }
+    }
 }
