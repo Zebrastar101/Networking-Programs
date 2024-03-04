84a2b96e22478e4af19b4d5c298939673b52fb6f
ChembsTacoma <chembiang3@gmail.com>
2024-03-04 09:37:04 -0600

    deleted main

--- ChatServer/ChatServer/src/CommandFromServer.java
+++ ChatServer/ChatServer/src/CommandFromServer.java
@@ -1,2 +1,40 @@
-public class CommandFromServer {
+
+import java.io.Serializable;
+public class CommandFromServer implements Serializable{
+    //The command being sent to the client
+    private int command;
+    //Text data for command
+    private String data="";
+
+    //Command list
+    public static final int CONNECT_AS_Y = 0;
+    public static final int CONNECT_AS_R = 1;
+    public static final int Y_TURN = 2;
+    public static final int R_TURN = 3;
+    public static final int MOVE = 4;
+    public static final int Y_WIN = 5;
+    public static final int R_WIN = 6;
+    public static final int TIE = 7;
+    public static final int RESTART=8;
+
+    public static final int QUIT = 9;
+
+
+
+    public CommandFromServer(int command, String data){
+        this.command = command;
+        this.data = data;
+    }
+
+    public int getCommand(){
+        return command;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+
+
 }
+
