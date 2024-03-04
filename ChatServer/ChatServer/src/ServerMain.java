22ece7368071ad9a7c53e7128d7e3443cfdec8a8
ChembsTacoma <chembiang3@gmail.com>
2024-03-04 10:20:51 -0600

    Update ServerMain.java

--- ChatServer/ChatServer/src/ServerMain.java
+++ ChatServer/ChatServer/src/ServerMain.java
@@ -9,37 +9,25 @@ public class ServerMain {
             // creates a socket that allows connections on port 8001
             ServerSocket serverSocket = new ServerSocket(8001);
 
-            // allow Y to connect and build streams to / from Y
-            Socket xCon = serverSocket.accept();
-            ObjectOutputStream xos = new ObjectOutputStream(xCon.getOutputStream());
-            ObjectInputStream xis = new ObjectInputStream(xCon.getInputStream());
-
-            // Lets the client know they are the Y player
-            xos.writeObject(new CommandFromServer(CommandFromServer.CONNECT_AS_Y,null));
-            System.out.println("Y has Connected.");
-
-            // Creates a Thread to listen to the Y client
-            ServersListener sl = new ServersListener(xis,xos,'Y');
-            Thread t = new Thread(sl);
-            t.start();
-
-            // allow R to connect and build streams to / from R
-            Socket oCon = serverSocket.accept();
-            ObjectOutputStream oos = new ObjectOutputStream(oCon.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(oCon.getInputStream());
-
-            // Lets the client know they are the R player
-            oos.writeObject(new CommandFromServer(CommandFromServer.CONNECT_AS_R,null));
-            System.out.println("R has Connected.");
-
-            // Creates a Thread to listen to the R client
-            sl = new ServersListener(ois,oos,'R');
-            t = new Thread(sl);
-            t.start();
+            while(true){
 
 
-            xos.writeObject(new CommandFromServer(CommandFromServer.Y_TURN,null));
-            oos.writeObject(new CommandFromServer(CommandFromServer.Y_TURN,null));
+                // allow Y to connect and build streams to / from Y
+                Socket xCon = serverSocket.accept();
+                ObjectOutputStream xos = new ObjectOutputStream(xCon.getOutputStream());
+                ObjectInputStream xis = new ObjectInputStream(xCon.getInputStream());
+
+                // Lets the client know they are the Y player
+                xos.writeObject(new CommandFromServer(CommandFromServer.NEWGUY, idk));
+                System.out.println("new person connected");
+
+                // Creates a Thread to listen to the Y client
+                ServersListener sl = new ServersListener(xis,xos,'idk');
+                Thread t = new Thread(sl);
+                t.start();
+            }
+
+
         }
         catch (Exception error){
             error.printStackTrace();
