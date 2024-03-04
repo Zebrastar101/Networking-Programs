2838ccbcc142dd85a088989a6c68eb848eb7aad2
ChembsTacoma <chembiang3@gmail.com>
2024-03-04 09:54:30 -0600

    Update ServerListener.java

--- ChatServer/ChatServer/src/ServerListener.java
+++ ChatServer/ChatServer/src/ServerListener.java
@@ -7,7 +7,7 @@ public class ServersListener implements Runnable{
     private ObjectOutputStream os = null;
 
     // Stores the which player this listener is for
-    private char player;
+    private char chatter;
 
     // static data that is shared between both listeners
     private static char turn = 'Y';
@@ -19,10 +19,10 @@ public class ServersListener implements Runnable{
 
 
 
-    public ServersListener(ObjectInputStream is, ObjectOutputStream os, char player) {
+    public ServersListener(ObjectInputStream is, ObjectOutputStream os, char chatter) {
         this.is = is;
         this.os = os;
-        this.player = player;
+        this.chatter = chatter;
         outs.add(os);
     }
 
@@ -35,54 +35,7 @@ public class ServersListener implements Runnable{
                 CommandFromClient cfc = (CommandFromClient) is.readObject();
 
                 //quit
-                if(cfc.getCommand()==CommandFromClient.QUIT){
-                    String data=cfc.getData();
-                    System.out.println("got quit, sending to clients");
-                    sendCommand(new CommandFromServer(CommandFromServer.QUIT,data));
-                }
 
-                // handle the received command
-                else if(cfc.getCommand()==CommandFromClient.MOVE &&
-                        turn==player && !gameData.isWinner('Y')
-                        && !gameData.isWinner('R')
-                        && !gameData.isCat())
-                {
-                    // pulls data for the move from the data field
-                    String data=cfc.getData();
-                    int c = data.charAt(0) - '0';
-                    int r = data.charAt(1) - '0';
-
-                    // if the move is invalid it, do not process it
-                    if(gameData.getGrid()[r][c]!=' ')
-                        continue;
-
-                    // changes the server side game board
-                    gameData.getGrid()[r][c] = player;
-
-                    // sends the move out to both players
-                    sendCommand(new CommandFromServer(CommandFromServer.MOVE,data));
-
-                    // changes the turn and checks to see if the game is over
-                    changeTurn();
-                    checkGameOver();
-                }
-                if(cfc.getCommand()==CommandFromClient.RESTART&&(gameData.isCat()||gameData.isWinner('Y')||gameData.isWinner('R'))){
-                    if (restar[0]==true){
-                        System.out.print("Both restarting");
-                        sendCommand(new CommandFromServer(CommandFromServer.RESTART, "Yas"));
-                        turn='Y';
-                        gameData.reset();
-                        restar[0]=false;
-
-                    }
-                    else if(restar[0]==false){
-                        System.out.print("Wanst to retart:"+player);
-                        restar[0]=true;
-                        sendCommand(new CommandFromServer(CommandFromServer.RESTART, ""+player));
-                    }
-                }
-
-            }
         }
         catch(Exception e)
         {
@@ -90,41 +43,16 @@ public class ServersListener implements Runnable{
         }
     }
 
-    public void changeTurn()
-    {
-        // changes the turn
-        if(turn=='Y')
-            turn = 'R';
-        else
-            turn ='Y';
 
-        // informs both client of the new player turn
-        if (turn == 'Y')
-            sendCommand(new CommandFromServer(CommandFromServer.Y_TURN, null));
-        else
-            sendCommand(new CommandFromServer(CommandFromServer.R_TURN, null));
-    }
 
-    public void checkGameOver()
-    {
-        int command = -1;
-        if(gameData.isCat())
-            command = CommandFromServer.TIE;
-        else if(gameData.isWinner('Y'))
-            command = CommandFromServer.Y_WIN;
-        else if(gameData.isWinner('R'))
-            command = CommandFromServer.R_WIN;
 
-        // if the game ended, informs both clients of the game's end state
-        if(command!=-1)
-            sendCommand(new CommandFromServer(command, null));
-    }
     public void sendCommand(CommandFromServer cfs)
     {
         // Sends command to both players
         for (ObjectOutputStream o : outs) {
             try {
                 o.writeObject(cfs);
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
