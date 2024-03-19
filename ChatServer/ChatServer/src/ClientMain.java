5d874b65d51ad38487b006fff62540c1c2b2e333
Zahraa <zahraatfatehi@gmail.com>
2024-03-19 09:15:01 -0500

    client stuff

--- ChatServer/ChatServer/src/ClientMain.java
+++ ChatServer/ChatServer/src/ClientMain.java
@@ -1,6 +1,7 @@
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientMain {
     public static void main(String[] args) {
@@ -10,15 +11,31 @@ public class ClientMain {
             Socket socket = new Socket("127.0.0.1", 8001);
             ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
             ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+            Scanner sc= new Scanner(System.in);
 
-            //determine if playing as X or O
             CommandFromServer cfs = (CommandFromServer)is.readObject();
-            ChatServer frame;
+            CSFrame frame;
 
 
+           boolean r=true;
+            while(r){
+                System.out.println("What is your name: ");
+                String name=sc.nextLine();
+                os.writeObject(os.writeObject(new CommandFromClient(CommandFromClient.CHECKNEWUSER, name ));
+                if(cfs.getCommand()==CommandFromServer.CHECKNEWUSER){
+                    r=cfs.getData();
+                    if(r==true){
+                        frame= new CSFrame(os, name);
+                        break;
+
+                    }
+                }
+            }
             ClientsListener cl = new ClientsListener(is, os, frame);
             Thread t = new Thread(cl);
             t.start();
+
+
         }
         catch(Exception error){
             error.printStackTrace();
