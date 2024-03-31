356a2df61ab00c0737fee8abb477ef1141964c6e
Zahraa <zahraatfatehi@gmail.com>
2024-03-31 18:47:17 -0500

    ss

--- School_Manager/SchoolManager/src/Main.java
+++ School_Manager/SchoolManager/src/Main.java
@@ -4,15 +4,18 @@ import javax.swing.*;
 public class Main {
    static Connection myConn;
     public static void main(String[] args) throws SQLException {
-        String url="jdbc:mysql://localhost:3306/db";
+        String url="jdbc:mysql://localhost:3306/managerschool";
         String user="root";
         String password="password";
         try{
             myConn=DriverManager.getConnection(url, user, password);
             Statement stm= myConn.createStatement();
-            stm.execute("CREATE DATABASE IF NOT EXISTS db");
-            stm.execute("USE db");
+            stm.execute("CREATE DATABASE IF NOT EXISTS managerschool");
+            stm.execute("USE managerschool");
             stm.execute("CREATE TABLE IF NOT EXISTS students(id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(id))");
+            stm.execute("CREATE TABLE IF NOT EXISTS teachers(id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(id))");
+            stm.execute("CREATE TABLE IF NOT EXISTS courses(id INTEGER NOT NULL AUTO_INCREMENT, course_name TEXT,type TEXT, PRIMARY KEY(id))");
+
         }
         catch(SQLException e){
             e.printStackTrace();
--- School_Manager/SchoolManager/src/Student.java
+++ School_Manager/SchoolManager/src/Student.java
@@ -5,11 +5,20 @@ public class Student {
     Connection con;
     Statement stm;
     public JTable studentTable;
+    ResultSet resultSet;
     public Student(Connection c){
         con =c;
         studentTable=new JTable();
+        try{
+            stm=con.createStatement();
+            stm.execute("USE managerschool");
+            resultSet=stm.executeQuery("Select*from students");
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+
+        }
 
     }
-    
+
 
 }
