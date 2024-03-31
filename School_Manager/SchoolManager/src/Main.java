71ddb64dc1d653c522e4619831e0bd1402cb8aa1
Zahraa <zahraatfatehi@gmail.com>
2024-03-31 12:30:16 -0500

    s

--- School_Manager/SchoolManager/src/Main.java
+++ School_Manager/SchoolManager/src/Main.java
@@ -1,5 +1,23 @@
+import java.sql.*;
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
+   static Connection myConn;
+    public static void main(String[] args) throws SQLException {
+        String url="jdbc:mysql://localhost:3306/db";
+        String user="root";
+        String password="password";
+        try{
+            myConn=DriverManager.getConnection(url, user, password);
+            Statement stm= myConn.createStatement();
+            stm.execute("CREATE DATABASE IF NOT EXISTS db");
+            stm.execute("USE db");
+            stm.execute("CREATE TABLE IF NOT EXISTS students(id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(id))");
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
         new SMFrame();
+
     }
 }
