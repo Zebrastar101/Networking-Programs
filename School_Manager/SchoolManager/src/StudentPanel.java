6c5e9d962b7fe899157170e998ed15fcd880a017
ChembsTacoma <chembiang3@gmail.com>
2024-04-01 11:41:20 -0500

    Update StudentPanel.java

--- School_Manager/SchoolManager/src/StudentPanel.java
+++ School_Manager/SchoolManager/src/StudentPanel.java
@@ -2,21 +2,22 @@ import javax.swing.*;
 
 public class StudentPanel extends JPanel {
 
-    JLabel studentFNLabel;
-    JLabel studentLNLabel;
+    JLabel panelTitleLabel = new JLabel("Students");
+    JLabel studentFNLabel = new JLabel("Student's first name: ");
+    JLabel studentLNLabel = new JLabel("Student's last name: ");
 
-    JTextField studentFNTextField;
-    JTextField studentLNTextField;
+    JTextField studentFNTextField = new JTextField("");
+    JTextField studentLNTextField = new JTextField("");
 
-    JTable ;
+    //JTable ;
 
-    JButton newButton;
-    JButton saveButton;
-    JButton deleteButton;
-    JButton scheduleButton;
+    JButton newButton = new JButton("New");
+    JButton saveButton = new JButton("Save");
+    JButton deleteButton = new JButton("Delete");
+    JButton scheduleButton = new JButton("Schedule");
 
 
     public StudentPanel(){
-
+        
     }
 }
