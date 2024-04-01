85d7ec742ea60ec1cc82ae285f4180e6c8eb24a8
ChembsTacoma <chembiang3@gmail.com>
2024-03-31 19:09:24 -0500

    Create StudentPanel.java

---
+++ School_Manager/SchoolManager/src/StudentPanel.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class StudentPanel extends JPanel {
+
+    JLabel studentFNLabel;
+    JLabel studentLNLabel;
+
+    JTextField studentFNTextField;
+    JTextField studentLNTextField;
+
+    JTable ;
+
+    JButton newButton;
+    JButton saveButton;
+    JButton deleteButton;
+    JButton scheduleButton;
+
+
+    public StudentPanel(){
+
+    }
+}
