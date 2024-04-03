9bbc8b5eccfc61f1e16b977b345ae4a22e1189d5
ChembsTacoma <chembiang3@gmail.com>
2024-04-03 09:53:13 -0500

    put zahraa's good sql code into my branch

--- School_Manager/SchoolManager/src/Student.java
+++ School_Manager/SchoolManager/src/Student.java
@@ -1,11 +1,9 @@
 import javax.swing.*;
-import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class Student {
-    /*
     Connection con;
     Statement stm;
     public JTable studentTable;
@@ -17,9 +15,9 @@ public class Student {
             stm=con.createStatement();
             stm.execute("USE managerschool");
             resultSet=stm.executeQuery("Select*from students");
-           studentTable.setModel((TableModel) buildTable(resultSet));
+            studentTable=buildTable(resultSet);
         }catch(SQLException e){
-            throw  new RuntimeException(e);
+            e.printStackTrace();
 
         }
 
@@ -27,26 +25,39 @@ public class Student {
     public JTable buildTable(ResultSet rs) throws SQLException {
         //make colums
         int colNum=rs.getMetaData().getColumnCount();
+        ArrayList<Object> perRow=new ArrayList<>();
+        ArrayList<ArrayList<Object>> data= new ArrayList<ArrayList<Object>>();
         ArrayList<String> colN= new ArrayList<>();
-        for(int x=1; x<=colNum;x++){
+
+      /*  for(int x=1; x<=colNum;x++){
             colN.add((String) rs.getObject(x));
         }
-        //make data
-        ArrayList<ArrayList<Object>> data= new ArrayList<ArrayList<Object>>();
-        while(rs.next()){
-            ArrayList<Object> perRow=new ArrayList<>();
+        //make data*/
+
+        while(rs.next()&&rs!=null){
+
             for(int z=1; z<=colNum; z++){
                 perRow.add(rs.getObject(z));
             }
             data.add(perRow);
         }
-        return new JTable((TableModel) data, (TableColumnModel) colN);
+        Object[][] dataArray= new Object[data.size()][data.get(0).size()];
+        for(int r=0; r< dataArray.length;r++){
+            for(int c=0; c<dataArray[0].length;c++){
+                dataArray[r][c]=data.get(r).get(c);
+                System.out.println(dataArray[r][c]);
+            }
+        }
+
+        JTable jTable = new JTable(dataArray, new String[]{"Student ID","First Name", "Last Name"});
+        return jTable;
     }
+
+
+
 
     public JTable getStudentTable() {
         return studentTable;
     }
-
-     */
 
 }
