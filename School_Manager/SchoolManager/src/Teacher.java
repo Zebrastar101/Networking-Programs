8c2fdc2c602d4cc3035b41e6cbce27ea0506e0b2
ChembsTacoma <chembiang3@gmail.com>
2024-04-07 15:59:14 -0500

    teacher stuff

--- School_Manager/SchoolManager/src/Teacher.java
+++ School_Manager/SchoolManager/src/Teacher.java
@@ -1,29 +1,117 @@
-public class Teacher {
-    private int teacherID;
-    private String teacherFirst_name;
-    private String teacherlast_name;
-    public Teacher(int id, String first_name, String last_name){
-        this.teacherID=id;
-        this.teacherFirst_name=first_name;
-        this.teacherlast_name=last_name;
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Teacher  {
+    Connection con;
+    Statement stm;
+    public JTable teacherTable;
+    ResultSet resultSet;
+    public Teacher(Connection c){
+        con =c;
+        //studentTable=new JTable();
+        try{
+            stm=con.createStatement();
+            resultSet=stm.executeQuery("Select*from teachers WHERE id >=1");
+
+            //the below while loop checks if there's elements in the resultSet
+            teacherTable=buildTable(resultSet);
+
+        }catch(SQLException e){
+            e.printStackTrace();
+
+        }
+
     }
-    public int getTeacherID(){
-        return this.teacherID;
+    public JTable buildTable(ResultSet rs) throws SQLException {
+        //make colums
+        int colNum=rs.getMetaData().getColumnCount();
+        ArrayList<Object> perRow=new ArrayList<>();
+        ArrayList<ArrayList<Object>> data= new ArrayList<ArrayList<Object>>();
+        ArrayList<String> colN= new ArrayList<>();
+
+      /*  for(int x=1; x<=colNum;x++){
+            colN.add((String) rs.getObject(x));
+        }
+        //make data*/
+
+        while(rs!=null && rs.next()){
+            for(int z=1; z<=colNum; z++){
+                perRow.add(rs.getObject(z));
+            }
+            data.add(perRow);
+
+
+            perRow=new ArrayList<>();
+        }
+        if(data.size()!=0){
+            Object[][] dataArray= new Object[data.size()][data.get(0).size()];
+            for(int r=0; r< dataArray.length;r++){
+                for(int c=0; c<dataArray[0].length;c++){
+                    dataArray[r] = data.get(r).toArray();
+                    //dataArray[r][c]=data.get(r).get(c);
+
+                }
+            }
+            System.out.println(Arrays.deepToString(dataArray));
+            return makeJTable(dataArray);
+        }
+
+
+        return makeJTable(new Object[0][0]);
     }
 
-    public String getTeacherFirst_name(){
-        return this.teacherFirst_name;
+    public JTable makeJTable(Object[][] dataArray){
+        DefaultTableModel tableModel = new DefaultTableModel(dataArray, new String[]{"Teacher ID","First Name", "Last Name"}) {
+
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                //all cells false
+                return false;
+            }
+        };
+
+        JTable table = new JTable();
+        table.setModel(tableModel);
+        table. getTableHeader().setReorderingAllowed(false);
+
+        return table;
     }
 
-    public String getTeacherLast_name() {
-        return teacherlast_name;
+
+
+
+    public JTable getTeacherTable() {
+        return teacherTable;
+    }
+    public JTable addTeacher(String fn, String ln) throws SQLException {
+        stm.executeUpdate("INSERT INTO teachers(first_name, last_name) VALUES('"+fn+"','"+ln+"');");
+        teacherTable=buildTable(stm.executeQuery("Select*from teachers WHERE id >=1"));
+        return teacherTable;
+
+    }
+    public JTable deleteStudent(String fn,String ln) throws SQLException{
+        stm.executeUpdate("DELETE FROM teachers WHERE first_name='"+fn+"'AND last_name='"+ln+"';");
+        teacherTable=buildTable(stm.executeQuery("Select*from teachers"));
+        return teacherTable;
+    }
+    public JTable purgeStudent() throws SQLException {
+        stm.execute("DROP TABLE IF EXISTS teachers;");
+        stm.execute("CREATE TABLE IF NOT EXISTS teachers(id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(id))");
+        teacherTable=buildTable(stm.executeQuery("Select*from teachers"));
+        return teacherTable;
+    }
+    public void exportStudent(){
+
     }
 
-    public void setTeacherFirst_name(String first_name) {
-        this.teacherFirst_name = first_name;
-    }
 
-    public void setLast_name(String last_name) {
-        this.teacherlast_name = last_name;
-    }
+
+
+
 }
--- School_Manager/SchoolManager/src/TeacherPanel.java
+++ School_Manager/SchoolManager/src/TeacherPanel.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TeacherPanel extends JPanel{
 
@@ -18,6 +20,8 @@ public class TeacherPanel extends JPanel{
     JButton saveButton = new JButton("Save");
     JButton deleteButton = new JButton("Delete");
     JButton sectionsButton = new JButton("Sections");
+
+    Teacher t;
 
 
     public TeacherPanel() {
@@ -64,5 +68,21 @@ public class TeacherPanel extends JPanel{
         sectionsButton.setBounds(400, 140, 90, 20);
         sectionsButton.setFont(new Font("Calibri", Font.BOLD, 10));
         add(sectionsButton);
+
+
+        t = new Teacher(Main.myConn);
+        teacherTable=t.getTeacherTable();
+        //below from https://www.tabnine.com/code/java/methods/javax.swing.JTable/getSelectedRow
+        teacherTable.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                String firstName = (String) teacherTable.getValueAt(teacherTable.getSelectedRow() , 1);
+                String lastName = (String) teacherTable.getValueAt(teacherTable.getSelectedRow() , 2);
+                teacherFNTextField.setText(firstName);
+                teacherLNTextField.setText(lastName);
+            }
+        });
+        jScrollPane = new JScrollPane(teacherTable);
+        jScrollPane.setBounds(50,190,500, 400);
+        add(jScrollPane);
     }
 }
