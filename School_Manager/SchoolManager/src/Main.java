53625638a1b45d2754d1fddf5f7bfd78c2ea4969
ChembsTacoma <chembiang3@gmail.com>
2024-04-09 09:20:51 -0500

    Update Main.java

--- School_Manager/SchoolManager/src/Main.java
+++ School_Manager/SchoolManager/src/Main.java
@@ -13,7 +13,6 @@ public class Main {
             stm.execute("CREATE DATABASE IF NOT EXISTS managerschool");
             stm.execute("USE managerschool");
             stm.execute("CREATE TABLE IF NOT EXISTS students(id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(id))");
-            //stm.executeUpdate("INSERT INTO students(first_name, last_name) VALUES('jim','smith');");
             stm.execute("CREATE TABLE IF NOT EXISTS teachers(id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(id))");
             stm.execute("CREATE TABLE IF NOT EXISTS courses(id INTEGER NOT NULL AUTO_INCREMENT, course_name TEXT,type TEXT, PRIMARY KEY(id))");
             stm.execute("CREATE TABLE IF NOT EXISTS sections(id INTEGER NOT NULL AUTO_INCREMENT, teacher_name TEXT,course_name TEXT, PRIMARY KEY(id))");
