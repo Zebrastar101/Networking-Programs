d8f9ab0a0869db97152b0667afa5b1cef25ca066
ChembsTacoma <chembiang3@gmail.com>
2024-04-15 09:57:47 -0500

    unused arraylist

--- School_Manager/SchoolManager/src/Student.java
+++ School_Manager/SchoolManager/src/Student.java
@@ -36,12 +36,7 @@ public class Student {
         int colNum=rs.getMetaData().getColumnCount();
         ArrayList<Object> perRow=new ArrayList<>();
         ArrayList<ArrayList<Object>> data= new ArrayList<ArrayList<Object>>();
-        ArrayList<String> colN= new ArrayList<>();
-
-      /*  for(int x=1; x<=colNum;x++){
-            colN.add((String) rs.getObject(x));
-        }
-        //make data*/
+        
 
         while(rs!=null && rs.next()){
             for(int z=1; z<=colNum; z++){
