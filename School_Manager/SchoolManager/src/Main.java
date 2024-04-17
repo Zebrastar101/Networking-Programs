9f81f9e3c66ff670d652d6a4a7643755420229ec
ChembsTacoma <chembiang3@gmail.com>
2024-04-17 10:21:17 -0500

    Update Main.java

--- School_Manager/SchoolManager/src/Main.java
+++ School_Manager/SchoolManager/src/Main.java
@@ -14,12 +14,12 @@ public class Main {
             stm.execute("USE managerschool");
             
             //stm.executeUpdate("INSERT INTO students(first_name, last_name) VALUES('jim','smith');");
-            stm.execute("CREATE TABLE IF NOT EXISTS teacher(teacher_id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(teacher_id))");
-            stm.execute("CREATE TABLE IF NOT EXISTS course(course_id INTEGER NOT NULL AUTO_INCREMENT, title TEXT,type TEXT, PRIMARY KEY(course_id))");
-            stm.execute("CREATE TABLE IF NOT EXISTS student(student_id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT,last_name TEXT, PRIMARY KEY(student_id))");
+            stm.execute("CREATE TABLE IF NOT EXISTS teacher(teacher_id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT, last_name TEXT, PRIMARY KEY(teacher_id))");
+            stm.execute("CREATE TABLE IF NOT EXISTS course(course_id INTEGER NOT NULL AUTO_INCREMENT, title TEXT, type TEXT, PRIMARY KEY(course_id))");
+            stm.execute("CREATE TABLE IF NOT EXISTS student(student_id INTEGER NOT NULL AUTO_INCREMENT, first_name TEXT, last_name TEXT, PRIMARY KEY(student_id))");
 
-            stm.execute("CREATE TABLE IF NOT EXISTS section(section_id INTEGER NOT NULL AUTO_INCREMENT, teacher_name TEXT,course_name TEXT, PRIMARY KEY(section_id))");
-            //stm.execute("CREATE TABLE IF NOT EXISTS enrollment(section_id INTEGER, student_id INTEGER, FOREIGN KEY(section_id),FOREIGN KEY(student_id)");
+            stm.execute("CREATE TABLE IF NOT EXISTS section(section_id INTEGER NOT NULL AUTO_INCREMENT, course_id INTEGER, teacher_id INTEGER, PRIMARY KEY(section_id), FOREIGN KEY(course_id), FOREIGN KEY(teacher_id))");
+            //stm.execute("CREATE TABLE IF NOT EXISTS enrollment(section_id INTEGER, student_id INTEGER, FOREIGN KEY(section_id),FOREIGN KEY(student_id))");
         }
         catch(SQLException e){
             e.printStackTrace();
