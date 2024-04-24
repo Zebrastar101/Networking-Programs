d772cfe2607187b20a64f1be3e10344b764ea3e9
Zahraa <zahraatfatehi@gmail.com>
2024-04-24 07:50:21 -0500

    Update StudentPanel.java

--- School_Manager/SchoolManager/src/StudentPanel.java
+++ School_Manager/SchoolManager/src/StudentPanel.java
@@ -252,7 +252,7 @@ public class StudentPanel extends JPanel {
     }
 
     public JTable makeJTable(Object[][] dataArray) {
-        System.out.println("found make Jtable");
+
         DefaultTableModel tableModel = new DefaultTableModel(dataArray, new String[]{"Section","Course","Teacher"}) {
 
             @Override
@@ -265,7 +265,7 @@ public class StudentPanel extends JPanel {
         JTable table = new JTable();
         table.setModel(tableModel);
         table.getTableHeader().setReorderingAllowed(false);
-        System.out.print(table);
+        
 
         return table;
     }
