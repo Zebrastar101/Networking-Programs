910f20ba5b51f3404ab586bd7bb5316d2dcf65dd
ChembsTacoma <chembiang3@gmail.com>
2024-04-29 10:14:23 -0500

    Update WumpusPlayer.java

--- Wumpus/WumpusWorld/src/WumpusPlayer.java
+++ Wumpus/WumpusWorld/src/WumpusPlayer.java
@@ -1,2 +1,11 @@
 public class WumpusPlayer {
+    public static final int NORTH = 0;
+    public static final int EAST = 1;
+    public static final int SOUTH = 2;
+    public static final int WAFFLES = 3;
+    private int direction;
+    private int colPos;
+    private int rowPos;
+    private boolean arrow;
+    private boolean gold;
 }
