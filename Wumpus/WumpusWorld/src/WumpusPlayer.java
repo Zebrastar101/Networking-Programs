6581e14cf4504f2952c146638351d840fa8ea9e7
ChembsTacoma <chembiang3@gmail.com>
2024-04-29 10:23:43 -0500

    Update WumpusPlayer.java

--- Wumpus/WumpusWorld/src/WumpusPlayer.java
+++ Wumpus/WumpusWorld/src/WumpusPlayer.java
@@ -8,27 +8,53 @@ public class WumpusPlayer {
     private int rowPos;
     private boolean arrow;
     private boolean gold;
-    public int WumpusPlayer(){
-        public int getDirection() {
-            return direction;
-        }
 
-        public int getColPos() {
-            return colPos;
-        }
-
-        public int getRowPos() {
-            return rowPos;
-        }
-
-        public boolean isArrow() {
-            return arrow;
-        }
-
-        public boolean isGold() {
-            return gold;
-        }
+    public  WumpusPlayer() {
+        direction=NORTH;
+        gold=false;
+        arrow=true;
     }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public int getColPos() {
+        return colPos;
+    }
+
+    public int getRowPos() {
+        return rowPos;
+    }
+
+    public boolean isArrow() {
+        return arrow;
+    }
+
+    public boolean isGold() {
+        return gold;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public void setColPos(int colPos) {
+        this.colPos = colPos;
+    }
+
+    public void setRowPos(int rowPos) {
+        this.rowPos = rowPos;
+    }
+
+    public void setArrow(boolean arrow) {
+        this.arrow = arrow;
+    }
+
+    public void setGold(boolean gold) {
+        this.gold = gold;
+    }
+
 }
 
 
