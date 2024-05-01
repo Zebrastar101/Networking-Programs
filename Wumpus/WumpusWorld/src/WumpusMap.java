9d7afc4b97bafeaf22710190c6bd0ddb55104996
ChembsTacoma <chembiang3@gmail.com>
2024-05-01 09:40:15 -0500

    Update WumpusMap.java

--- Wumpus/WumpusWorld/src/WumpusMap.java
+++ Wumpus/WumpusWorld/src/WumpusMap.java
@@ -1,2 +1,35 @@
 public class WumpusMap {
+    public static final int NUM_ROWS = 10;
+    public static final int NUM_COLUMNS = 10;
+    public static final int NUM_PITS = 10;
+    private WumpusSquare[][] grid;
+    private int ladderColumn;
+    private int ladderRow;
+
+    public WumpusMap(){
+        createMap();
+    }
+
+    public void createMap(){
+
+    }
+
+    public int getLadderColumn() {
+        return ladderColumn;
+    }
+
+    public int getLadderRow() {
+        return ladderRow;
+    }
+
+    public WumpusSquare getSquare(int col, int row){
+
+        return null;
+    }
+
+    public String toString(){
+
+        return null;
+    }
+
 }
