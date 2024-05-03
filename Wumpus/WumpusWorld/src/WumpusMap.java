03f02ae485b69137717ac35b4b732b019249c9a3
ChembsTacoma <chembiang3@gmail.com>
2024-05-03 10:17:11 -0500

    Update WumpusMap.java

--- Wumpus/WumpusWorld/src/WumpusMap.java
+++ Wumpus/WumpusWorld/src/WumpusMap.java
@@ -11,32 +11,76 @@ public class WumpusMap {
     }
 
     public void createMap(){
+        grid= new WumpusSquare[10][10];
         for(int r = 0; r<=9; r++){
             for(int c = 0; c<=9; c++){
                 grid[r][c]= new WumpusSquare();
             }
         }
-        grid= new WumpusSquare[10][10];
-        for(int r = 0; r<=9; r++){
-            for(int c = 0; c<=9; c++){
-                for(int i=1; i<=10; i++){
-                    //pits
-                    //int randomNum = min + (int)(Math.random() * ((max – min) + 1));
-                    int randRow = 0+(int)(Math.random()*((9-0)+1));
-                    int randCol = 0+(int)(Math.random()*((9-0)+1));
-                    while(grid[randRow][randCol].isPit() || grid[randRow][randCol].isBreeze()){
-                        randRow = 0+(int)(Math.random()*((9-0)+1));
-                        randCol = 0+(int)(Math.random()*((9-0)+1));
-                    }
-                    grid[r][c].setPit(true);
-                    grid[r+1][c].setBreeze(true);
-                    grid[r-1][c].setBreeze(true);
-                    grid[r][c+1].setBreeze(true);
-                    grid[r][c-1].setBreeze(true);
-                    
-                }
+
+        for(int i=1; i<=10; i++){
+            // 10 pits & breezes
+            //int randomNum = min + (int)(Math.random() * ((max – min) + 1));
+            int randRow = 0+(int)(Math.random()*((9-0)+1));
+            int randCol = 0+(int)(Math.random()*((9-0)+1));
+            while(grid[randRow][randCol].isPit()){
+                randRow = 0+(int)(Math.random()*((9-0)+1));
+                randCol = 0+(int)(Math.random()*((9-0)+1));
             }
+            grid[randRow][randCol].setPit(true);
+            if(!grid[randRow+1][randCol].isPit()){
+                grid[randRow+1][randCol].setBreeze(true);
+            }
+            if(!grid[randRow-1][randCol].isPit()){
+                grid[randRow-1][randCol].setBreeze(true);
+            }
+            if(!grid[randRow][randCol+1].isPit()){
+                grid[randRow][randCol+1].setBreeze(true);
+            }
+            if(!grid[randRow][randCol-1].isPit()){
+                grid[randRow][randCol-1].setBreeze(true);
+            }
+
+
         }
+        //gold
+        int randRow = 0+(int)(Math.random()*((9-0)+1));
+        int randCol = 0+(int)(Math.random()*((9-0)+1));
+        while(grid[randRow][randCol].isPit() || grid[randRow][randCol].isLadder()){
+            randRow = 0+(int)(Math.random()*((9-0)+1));
+            randCol = 0+(int)(Math.random()*((9-0)+1));
+        }
+        grid[randRow][randCol].setGold(true);
+
+        //wumpus and Stenches
+        randRow = 0+(int)(Math.random()*((9-0)+1));
+        randCol = 0+(int)(Math.random()*((9-0)+1));
+        while(grid[randRow][randCol].isPit() || grid[randRow][randCol].isLadder()){
+            randRow = 0+(int)(Math.random()*((9-0)+1));
+            randCol = 0+(int)(Math.random()*((9-0)+1));
+        }
+        grid[randRow][randCol].setWumpus(true);
+        if(!grid[randRow+1][randCol].isPit()){
+            grid[randRow+1][randCol].setStench(true);
+        }
+        if(!grid[randRow-1][randCol].isPit()){
+            grid[randRow-1][randCol].setStench(true);
+        }
+        if(!grid[randRow][randCol+1].isPit()){
+            grid[randRow][randCol+1].setStench(true);
+        }
+        if(!grid[randRow][randCol-1].isPit()){
+            grid[randRow][randCol-1].setStench(true);
+        }
+
+        //ladder
+        randRow = 0+(int)(Math.random()*((9-0)+1));
+        randCol = 0+(int)(Math.random()*((9-0)+1));
+        while(grid[randRow][randCol].isPit() || grid[randRow][randCol].isGold() || grid[randRow][randCol].isWumpus()){
+            randRow = 0+(int)(Math.random()*((9-0)+1));
+            randCol = 0+(int)(Math.random()*((9-0)+1));
+        }
+        grid[randRow][randCol].setLadder(true);
     }
 
     public int getLadderColumn() {
