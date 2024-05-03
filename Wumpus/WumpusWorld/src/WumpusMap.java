07690067ec2595818496be4ee555082d55372553
ChembsTacoma <chembiang3@gmail.com>
2024-05-03 09:24:57 -0500

    Update WumpusMap.java

--- Wumpus/WumpusWorld/src/WumpusMap.java
+++ Wumpus/WumpusWorld/src/WumpusMap.java
@@ -13,7 +13,7 @@ public class WumpusMap {
     public void createMap(){
         for(int r = 0; r<=9; r++){
             for(int c = 0; c<=9; c++){
-                grid[r][c]= new WumpusSquare() idk;
+                grid[r][c]= new WumpusSquare();
             }
         }
         grid= new WumpusSquare[10][10];
@@ -24,12 +24,16 @@ public class WumpusMap {
                     //int randomNum = min + (int)(Math.random() * ((max – min) + 1));
                     int randRow = 0+(int)(Math.random()*((9-0)+1));
                     int randCol = 0+(int)(Math.random()*((9-0)+1));
-                    while(grid[randRow][randCol]){
-
+                    while(grid[randRow][randCol].isPit() || grid[randRow][randCol].isBreeze()){
+                        randRow = 0+(int)(Math.random()*((9-0)+1));
+                        randCol = 0+(int)(Math.random()*((9-0)+1));
                     }
-
-
-
+                    grid[r][c].setPit(true);
+                    grid[r+1][c].setBreeze(true);
+                    grid[r-1][c].setBreeze(true);
+                    grid[r][c+1].setBreeze(true);
+                    grid[r][c-1].setBreeze(true);
+                    
                 }
             }
         }
