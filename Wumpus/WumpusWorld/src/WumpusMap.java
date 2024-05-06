e917a51a583fd1466d9afb9fbc71c5ae6b5cad8e
ChembsTacoma <chembiang3@gmail.com>
2024-05-06 10:04:49 -0500

    finished wumpusMAp

--- Wumpus/WumpusWorld/src/WumpusMap.java
+++ Wumpus/WumpusWorld/src/WumpusMap.java
@@ -8,6 +8,7 @@ public class WumpusMap {
 
     public WumpusMap(){
         createMap();
+        System.out.println(this);
     }
 
     public void createMap(){
@@ -28,18 +29,27 @@ public class WumpusMap {
                 randCol = 0+(int)(Math.random()*((9-0)+1));
             }
             grid[randRow][randCol].setPit(true);
-            if(!grid[randRow+1][randCol].isPit()){
-                grid[randRow+1][randCol].setBreeze(true);
+            if(randRow+1<=9){
+                if(!grid[randRow+1][randCol].isPit()){
+                    grid[randRow+1][randCol].setBreeze(true);
+                }
             }
-            if(!grid[randRow-1][randCol].isPit()){
-                grid[randRow-1][randCol].setBreeze(true);
+            if(randRow-1>=0){
+                if(!grid[randRow-1][randCol].isPit()){
+                    grid[randRow-1][randCol].setBreeze(true);
+                }
             }
-            if(!grid[randRow][randCol+1].isPit()){
-                grid[randRow][randCol+1].setBreeze(true);
+            if(randCol+1<=9){
+                if(!grid[randRow][randCol+1].isPit()){
+                    grid[randRow][randCol+1].setBreeze(true);
+                }
             }
-            if(!grid[randRow][randCol-1].isPit()){
-                grid[randRow][randCol-1].setBreeze(true);
+            if(randCol-1>=0){
+                if(!grid[randRow][randCol-1].isPit()){
+                    grid[randRow][randCol-1].setBreeze(true);
+                }
             }
+
 
 
         }
@@ -60,17 +70,25 @@ public class WumpusMap {
             randCol = 0+(int)(Math.random()*((9-0)+1));
         }
         grid[randRow][randCol].setWumpus(true);
-        if(!grid[randRow+1][randCol].isPit()){
-            grid[randRow+1][randCol].setStench(true);
+        if(randRow+1<=9){
+            if(!grid[randRow+1][randCol].isPit()){
+                grid[randRow+1][randCol].setStench(true);
+            }
         }
-        if(!grid[randRow-1][randCol].isPit()){
-            grid[randRow-1][randCol].setStench(true);
+        if(randRow-1>=0){
+            if(!grid[randRow-1][randCol].isPit()){
+                grid[randRow-1][randCol].setStench(true);
+            }
         }
-        if(!grid[randRow][randCol+1].isPit()){
-            grid[randRow][randCol+1].setStench(true);
+        if(randCol+1<=9){
+            if(!grid[randRow][randCol+1].isPit()){
+                grid[randRow][randCol+1].setStench(true);
+            }
         }
-        if(!grid[randRow][randCol-1].isPit()){
-            grid[randRow][randCol-1].setStench(true);
+        if(randCol-1>=0){
+            if(!grid[randRow][randCol-1].isPit()){
+                grid[randRow][randCol-1].setStench(true);
+            }
         }
 
         //ladder
@@ -94,13 +112,23 @@ public class WumpusMap {
     }
 
     public WumpusSquare getSquare(int col, int row){
-
-        return null;
+        if(row>=0 && row<=9 && col>=0 && col<=9){
+            return null;
+        }
+        return grid[row][col];
     }
 
     public String toString(){
-
-        return null;
+        //need to do breeze things
+        String string = "";
+        for(int r = 0; r<=9; r++){
+            for(int c = 0; c<=9; c++){
+                string+=grid[r][c].toString() + " ";
+            }
+            string+= "\n";
+        }
+        return string;
     }
+
 
 }
