bd8a19e27b5200bce6f46c6ab690b35d00a51e9e
Zahraa <zahraatfatehi@gmail.com>
2024-05-08 09:09:16 -0500

    f

--- Wumpus/WumpusWorld/src/Main.java
+++ Wumpus/WumpusWorld/src/Main.java
@@ -3,6 +3,6 @@ import javax.imageio.IIOException;
 public class Main {
     public static void main(String[] args) throws IIOException {
         WumpusFrame frame=new WumpusFrame();
-        frame.setVisible(true);
+       
     }
 }
--- Wumpus/WumpusWorld/src/WumpusFrame.java
+++ Wumpus/WumpusWorld/src/WumpusFrame.java
@@ -5,7 +5,7 @@ public class WumpusFrame extends JFrame {
     public WumpusFrame() throws IIOException {
         setSize(801,801);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocale(null);
+        setLayout(null);
         WumpusPanel p= new WumpusPanel();
         add(p);
         setVisible(true);
