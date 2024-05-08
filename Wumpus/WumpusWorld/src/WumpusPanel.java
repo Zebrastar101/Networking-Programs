d4a3c841da4ca1e15bc5311ded4e4e705c9809aa
Zahraa <zahraatfatehi@gmail.com>
2024-05-08 08:24:32 -0500

    da

--- Wumpus/WumpusWorld/src/WumpusPanel.java
+++ Wumpus/WumpusWorld/src/WumpusPanel.java
@@ -126,63 +126,96 @@ public class WumpusPanel extends JPanel implements KeyListener{
     @Override
     public void keyTyped(KeyEvent e) {
         char event=e.getKeyChar();
-        if (event == 'W' || event == 'w') {
+        if ((event == 'W' || event == 'w') &&status==PLAYING){
             player.setRowPos(player.getRowPos()+1);
             map.getSquare(player.getRowPos(),player.getColPos()).setVisited(true);
             player.setDirection(0);
             repaint();
         }
-        if (event == 'D' || event == 'd') {
+        if ((event == 'D' || event == 'd') &&status==PLAYING){
             player.setColPos(player.getColPos()+1);
             map.getSquare(player.getRowPos(),player.getColPos()).setVisited(true);
             player.setDirection(WumpusPlayer.EAST);
             repaint();
 
         }
-        if ((event == 'S' || event == 's')&&player.getRowPos()>0) {
+        if ((event == 'S' || event == 's')&&player.getRowPos()>0&&status==PLAYING) {
             player.setRowPos(player.getRowPos()-1);
             map.getSquare(player.getRowPos(),player.getColPos()).setVisited(true);
             player.setDirection(WumpusPlayer.SOUTH);
             repaint();
 
         }
-        if ((event == 'a' || event == 'A')&&player.getColPos()>0) {
+        if ((event == 'a' || event == 'A')&&player.getColPos()>0&&status==PLAYING) {
             player.setColPos(player.getColPos()-1);
             map.getSquare(player.getRowPos(),player.getColPos()).setVisited(true);
             player.setDirection(WumpusPlayer.WEST);
             repaint();
         }
-        if ((event == 'i' || event == 'I')&&player.isArrow()==true) {
+        if ((event == 'i' || event == 'I')&&player.isArrow()==true&&status==PLAYING) {
             //nio clue how to shoot arrow yet;
             player.setArrow(false);
-            
+            for(int x=player.getRowPos()+1; x<10;x++){
+                if(map.getSquare(x,player.getColPos()).isWumpus()==true){
+                    map.getSquare(x,player.getColPos()).setWumpus(false);
+                    map.getSquare(x,player.getColPos()).setDeadWumpus(true);
+
+                }
+            }
+            repaint();
 
         }
-        if ((event == 'l' || event == 'L')&&player.isArrow()==true) {
+        if ((event == 'l' || event == 'L')&&player.isArrow()==true&&status==PLAYING) {
             //nio clue how to shoot arrow yet;
             player.setArrow(false);
+            for(int x=player.getColPos()+1; x<10;x++){
+                if(map.getSquare(player.getRowPos(),x).isWumpus()==true){
+                    map.getSquare(player.getRowPos(),x).setWumpus(false);
+                    map.getSquare(player.getRowPos(),x).setDeadWumpus(true);
+                    repaint();
+                }
+            }
+            repaint();
         }
-        if ((event == 'k' || event == 'K')&&player.isArrow()==true) {
+        if ((event == 'k' || event == 'K')&&player.isArrow()==true&&status==PLAYING) {
             //nio clue how to shoot arrow yet;
             player.setArrow(false);
+            for(int x=player.getRowPos()+1; x<10;x++){
+                if(map.getSquare(x,player.getColPos()).isWumpus()==true){
+                    map.getSquare(x,player.getColPos()).setWumpus(false);
+                    map.getSquare(x,player.getColPos()).setDeadWumpus(true);
+
+                }
+            }
+            repaint();
         }
-        if ((event == 'j' || event == 'J')&&player.isArrow()==true) {
+        if ((event == 'j' || event == 'J')&&player.isArrow()==true&&status==PLAYING) {
             //nio clue how to shoot arrow yet;
             player.setArrow(false);
+            for(int x=player.getColPos()-1; x>-1;x--){
+                if(map.getSquare(player.getRowPos(),x).isWumpus()==true){
+                    map.getSquare(player.getRowPos(),x).setWumpus(false);
+                    map.getSquare(player.getRowPos(),x).setDeadWumpus(true);
+
+                }
+            }
+            repaint();
         }
-        if ((event == 'c' || event == 'C')&&player.isGold()==true&&player.getColPos()==map.getLadderColumn()&&player.getRowPos()==map.getLadderRow()) {
+        if ((event == 'c' || event == 'C')&&player.isGold()==true&&player.getColPos()==map.getLadderColumn()&&player.getRowPos()==map.getLadderRow()&&status==PLAYING) {
             //nio clue how to shoot arrow yet;
             status=WON;
+            repaint();
         }
-        if(map.getSquare(player.getRowPos(),player.getColPos()).isPit()||map.getSquare(player.getRowPos(),player.getColPos()).isWumpus()){
+        if(map.getSquare(player.getRowPos(),player.getColPos()).isPit()||map.getSquare(player.getRowPos(),player.getColPos()).isWumpus()&&status==PLAYING){
             status=DEAD;
+            repaint();
         }
         if ((event == 'n' || event == 'N')&&status!=PLAYING) {
             //nio clue how to shoot arrow yet;
             reset();
 
         }
-        if (event=='*') {
+        if (event=='*'&&status==PLAYING) {
             if(cheat==true){
                 cheat=false;
             }else{
@@ -190,7 +223,6 @@ public class WumpusPanel extends JPanel implements KeyListener{
             }
             repaint();
         }
-
 
     }
 
